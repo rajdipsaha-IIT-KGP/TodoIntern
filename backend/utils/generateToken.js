aeee6b6697b55b5dcf29c1bee1cbf361430c6549
Rajdip <rajdipsaha7697@gmail.com>
2026-02-06 21:32:34 +0530

    adding https

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,12 +1,16 @@
-const jwt = require('jsonwebtoken');
-const generateToken = (id,res) =>{
-   const token = jwt.sign({id}, process.env.JWT_SECRET,{
-       expiresIn: '15d',
-   });
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id, res) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+
   res.cookie("token", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    secure: true,        
+    sameSite: "none",    
   });
 };
+
 module.exports = generateToken;
